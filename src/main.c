346b78a1782a1130ac24b08a4a2deb63d755c5f1
Tim Vrakas <tim.vrakas@gmail.com>
2019-02-03 03:56:19 -0800

    minor tweaks

--- src/main.c
+++ src/main.c
@@ -5,12 +5,21 @@ int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
-	volatile int lol;
 
 	cdcd_acm_example();
 	/* Replace with your application code */
 	
+	gpio_set_pin_level(LED1,true);
+	gpio_set_pin_level(LED2,true);
+	gpio_set_pin_level(LED3,true);
+	gpio_set_pin_level(LED4,true);
 	
+	for(int i=0;i<100000;i++){}
+
+	gpio_set_pin_level(LED1,false);
+	gpio_set_pin_level(LED2,false);
+	gpio_set_pin_level(LED3,false);
+	gpio_set_pin_level(LED4,false);
 	
 	while(true){
 		for(int j=0;j<20;j++){
@@ -18,9 +27,7 @@ int main(void)
 			gpio_toggle_pin_level(LED4);
 			
 			for(int k=0;k<100;k++){
-				for(int i=0;i<10000;i++){
-					lol = i;
-				}
+				for(int i=0;i<10000;i++){}
 				gpio_toggle_pin_level(BUZZER);
 			}
 			printf("TopKekTest\n");
