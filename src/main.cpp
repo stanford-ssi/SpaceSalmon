996dd2eeced917c0de8247670692369517a72681
Tim Vrakas <tim.vrakas@gmail.com>
2019-03-19 00:08:43 -0700

    BMP388 Works!

--- src/main.cpp
+++ src/main.cpp
@@ -102,7 +102,7 @@ int main(void)
 		}
 
 		//test BMP388
-		//gpio_set_pin_level(LED3, testBMPSpi());
+		gpio_set_pin_level(LED3, testBMPSpi());
 
 		//for (int i = 0; i < 1000; i++){}
 
@@ -168,9 +168,9 @@ bool testADXLSpi()
 
 bool testBMPSpi()
 {
-	uint8_t send[] = {0x80, 0x00, 0x00, 0x00};
-	uint8_t recv[] = {0x00, 0x00, 0x00, 0x00};
-	spi_xfer data = {send, recv, 4};
+	uint8_t send[] = {0x80, 0x00, 0x00};
+	uint8_t recv[] = {0x00, 0x00, 0x00};
+	spi_xfer data = {send, recv, 3};
 
 	spi_m_sync_disable(&SPI_SENSOR);
 	spi_m_sync_set_mode(&SPI_SENSOR, SPI_MODE_3);
@@ -180,8 +180,8 @@ bool testBMPSpi()
 	spi_m_sync_transfer(&SPI_SENSOR, &data);
 	gpio_set_pin_level(BMP_CS_1, true);
 	
-	printf("BMP388 Response: 0x%x 0x%x 0x%x 0x%x\n", data.rxbuf[0], data.rxbuf[1], data.rxbuf[2], data.rxbuf[3]);
-	return data.rxbuf[0] == 0x50;
+	printf("BMP388 Response: 0x%x 0x%x 0x%x\n", data.rxbuf[0], data.rxbuf[1], data.rxbuf[2]);
+	return data.rxbuf[2] == 0x50;
 }
 
 bool testSquibSpi()
@@ -192,7 +192,7 @@ bool testSquibSpi()
 	spi_m_sync_disable(&SPI_SQUIB);
 	spi_m_sync_set_mode(&SPI_SQUIB, SPI_MODE_0);
 	spi_m_sync_enable(&SPI_SQUIB);
-	
+
 	gpio_set_pin_level(SQUIB_CS, false);
 	spi_m_sync_transfer(&SPI_SQUIB, &data);
 	gpio_set_pin_level(SQUIB_CS, true);
