6969b49f4abead636205f153bb865eeaf57a4746
Tim Vrakas <tim.vrakas@gmail.com>
2019-03-19 03:07:06 -0700

    now ADXL is a reall library

--- lib/ADXL375/ADXL375.cpp
+++ lib/ADXL375/ADXL375.cpp
@@ -1,11 +1,9 @@
-extern "C" {
-#include <atmel_start.h>
-}
-
 #include "ADXL375.hpp"
 
-ADXL375::ADXL375()
+ADXL375::ADXL375(struct spi_m_sync_descriptor *SPI, uint8_t CS_PIN)
 {
+  this->SPI = SPI;
+  this->CS_PIN = CS_PIN;
 }
 
 void ADXL375::init()
@@ -30,10 +28,10 @@ AccelData ADXL375::getXYZ()
   _multiReadRegister(ADXL375_REG_DATAX0, data, 6);
 
   AccelData xyz;
-  xyz.x = ((int16_t)(data[0] | data[1]<<8))*ADXL375_XYZ_READ_SCALE_FACTOR;
-  xyz.y = ((int16_t)(data[2] | data[3]<<8))*ADXL375_XYZ_READ_SCALE_FACTOR;
-  xyz.z = ((int16_t)(data[4] | data[5]<<8))*ADXL375_XYZ_READ_SCALE_FACTOR;
-  
+  xyz.x = ((int16_t)(data[0] | data[1] << 8)) * ADXL375_XYZ_READ_SCALE_FACTOR;
+  xyz.y = ((int16_t)(data[2] | data[3] << 8)) * ADXL375_XYZ_READ_SCALE_FACTOR;
+  xyz.z = ((int16_t)(data[4] | data[5] << 8)) * ADXL375_XYZ_READ_SCALE_FACTOR;
+
   return xyz;
 }
 
@@ -59,7 +57,7 @@ uint8_t ADXL375::getFIFOBufferSize()
 
 void ADXL375::startShockDetection()
 {
-  setShockAxes(true,true,true);
+  setShockAxes(true, true, true);
 
   // shock duration to test for 625 μs/LSB
   writeRegister(ADXL375_REG_DUR, 2);
@@ -78,9 +76,12 @@ void ADXL375::startShockDetection()
 void ADXL375::setShockAxes(bool x, bool y, bool z)
 {
   uint8_t shockAxesData = 0b00000000;
-  if(x) shockAxesData |= 0b100;
-  if(y) shockAxesData |= 0b010;
-  if(z) shockAxesData |= 0b001;
+  if (x)
+    shockAxesData |= 0b100;
+  if (y)
+    shockAxesData |= 0b010;
+  if (z)
+    shockAxesData |= 0b001;
 
   // the axes we want to detect shocks on
   writeRegister(ADXL375_REG_SHOCK_AXES, shockAxesData);
@@ -88,7 +89,7 @@ void ADXL375::setShockAxes(bool x, bool y, bool z)
 
 void ADXL375::setFIFOMode(uint8_t mode, uint8_t pin, uint8_t samples)
 {
-  writeRegister(ADXL375_REG_FIFO_CTL, mode<<6 | pin<<5 | samples);
+  writeRegister(ADXL375_REG_FIFO_CTL, mode << 6 | pin << 5 | samples);
 }
 
 void ADXL375::_multiReadRegister(uint8_t regAddress, uint8_t values[], uint8_t numberOfBytes)
@@ -97,54 +98,54 @@ void ADXL375::_multiReadRegister(uint8_t regAddress, uint8_t values[], uint8_t n
   regAddress |= 0x80;
 
   // set the multi-read byte if required
-  if (numberOfBytes > 1) {
+  if (numberOfBytes > 1)
+  {
     regAddress |= 0x40;
   }
 
   uint8_t send[numberOfBytes + 1];
-	uint8_t recv[numberOfBytes + 1];
+  uint8_t recv[numberOfBytes + 1];
 
-  memset(send, 0x00, numberOfBytes+1);
-  memset(recv, 0x00, numberOfBytes+1);
+  memset(send, 0x00, numberOfBytes + 1);
+  memset(recv, 0x00, numberOfBytes + 1);
 
   send[0] = regAddress;
 
   struct spi_xfer data;
 
-  data.size = numberOfBytes+1;
-	data.txbuf = send;
-	data.rxbuf = recv;
+  data.size = numberOfBytes + 1;
+  data.txbuf = send;
+  data.rxbuf = recv;
 
-  spi_m_sync_disable(&SPI_SENSOR);
-	spi_m_sync_set_mode(&SPI_SENSOR, SPI_MODE_3);
-	spi_m_sync_enable(&SPI_SENSOR);
+  //TODO: this could be removed...
+  spi_m_sync_disable(SPI);
+  spi_m_sync_set_mode(SPI, SPI_MODE_3);
+  spi_m_sync_enable(SPI);
 
-  gpio_set_pin_level(ADXL_CS_1,false);
-	spi_m_sync_transfer(&SPI_SENSOR, &data);
-  gpio_set_pin_level(ADXL_CS_1,true);
+  gpio_set_pin_level(CS_PIN, false);
+  spi_m_sync_transfer(SPI, &data);
+  gpio_set_pin_level(CS_PIN, true);
 
-  memcpy(values,recv+1,numberOfBytes);
-  
+  memcpy(values, recv + 1, numberOfBytes);
 }
 
 void ADXL375::writeRegister(uint8_t regAddress, uint8_t value)
 {
-  uint8_t send[] = {regAddress,value};
-	uint8_t recv[] = {0x00, 0x00};
+  uint8_t send[] = {regAddress, value};
+  uint8_t recv[] = {0x00, 0x00};
 
   struct spi_xfer data;
 
   data.size = 2;
-	data.txbuf = send;
-	data.rxbuf = recv;
-  
+  data.txbuf = send;
+  data.rxbuf = recv;
+
   //TODO: this could be removed...
-  spi_m_sync_disable(&SPI_SENSOR);
-	spi_m_sync_set_mode(&SPI_SENSOR, SPI_MODE_3);
-	spi_m_sync_enable(&SPI_SENSOR);
+  spi_m_sync_disable(SPI);
+  spi_m_sync_set_mode(SPI, SPI_MODE_3);
+  spi_m_sync_enable(SPI);
 
-  gpio_set_pin_level(ADXL_CS_1,false);
-	spi_m_sync_transfer(&SPI_SENSOR, &data);
-  gpio_set_pin_level(ADXL_CS_1,true);
+  gpio_set_pin_level(CS_PIN, false);
+  spi_m_sync_transfer(SPI, &data);
+  gpio_set_pin_level(CS_PIN, true);
 }
-
---
+++ lib/ADXL375/ADXL375.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+extern "C"
+{
+#include <hal_spi_m_sync.h>
+#include <hal_gpio.h>
+#include <stdint.h>
+#include <string.h>
+}
+
+#define ADXL375_REG_DEVID 0x00            // Device ID
+#define ADXL375_REG_THRESH_SHOCK 0x1D     // Shock threshold
+#define ADXL375_REG_OFSX 0x1E             // X-axis offset
+#define ADXL375_REG_OFSY 0x1F             // Y-axis offset
+#define ADXL375_REG_OFSZ 0x20             // Z-axis offset
+#define ADXL375_REG_DUR 0x21              // Shock duration
+#define ADXL375_REG_LATENT 0x22           // Shock latency
+#define ADXL375_REG_WINDOW 0x23           // Tap window
+#define ADXL375_REG_THRESH_ACT 0x24       // Activity threshold
+#define ADXL375_REG_THRESH_INACT 0x25     // Inactivity threshold
+#define ADXL375_REG_TIME_INACT 0x26       // Inactivity time
+#define ADXL375_REG_ACT_INACT_CTL 0x27    // Axis enable control for activity and inactivity detection
+#define ADXL375_REG_SHOCK_AXES 0x2A       // Axis control for single/double tap
+#define ADXL375_REG_ACT_SHOCK_STATUS 0x2B // Source for single/double tap
+#define ADXL375_REG_BW_RATE 0x2C          // Data rate and power mode control
+#define ADXL375_REG_POWER_CTL 0x2D        // Power-saving features control
+#define ADXL375_REG_INT_ENABLE 0x2E       // Interrupt enable control
+#define ADXL375_REG_INT_MAP 0x2F          // Interrupt mapping control
+#define ADXL375_REG_INT_SOURCE 0x30       // Source of interrupts
+#define ADXL375_REG_DATA_FORMAT 0x31      // Data format control
+#define ADXL375_REG_DATAX0 0x32           // X-axis data 0
+#define ADXL375_REG_DATAX1 0x33           // X-axis data 1
+#define ADXL375_REG_DATAY0 0x34           // Y-axis data 0
+#define ADXL375_REG_DATAY1 0x35           // Y-axis data 1
+#define ADXL375_REG_DATAZ0 0x36           // Z-axis data 0
+#define ADXL375_REG_DATAZ1 0x37           // Z-axis data 1
+#define ADXL375_REG_FIFO_CTL 0x38         // FIFO control
+#define ADXL375_REG_FIFO_STATUS 0x39      // FIFO status
+
+#define ADXL375_XYZ_READ_SCALE_FACTOR 49 // scaling factor when reading xyz data
+#define ADXL375_THRESH_SHOCK_SCALE 780   // scaling factor for shock threshold register
+
+#define ADXL375_FIFO_MODE_BYPASS 0b00
+#define ADXL375_FIFO_MODE_FIFO 0b01
+#define ADXL375_FIFO_MODE_STREAM 0b10
+#define ADXL375_FIFO_MODE_TRIGGER 0b11
+
+#define ADXL375_TRIGGER_INT1_PIN 0
+#define ADXL375_TRIGGER_INT2_PIN 1
+
+struct AccelData
+{
+  int32_t x;
+  int32_t y;
+  int32_t z;
+};
+
+enum ADXL375_Bandwidth
+{
+  BW_0_05HZ = 0,
+  BW_0_1HZ = 1,
+  BW_0_2HZ = 2,
+  BW_0_39HZ = 3,
+  BW_0_78HZ = 4,
+  BW_1_56HZ = 5,
+  BW_3_13HZ = 6,
+  BW_6_25HZ = 7,
+  BW_12_5HZ = 8,
+  BW_25HZ = 9,
+  BW_50HZ = 10,
+  BW_100HZ = 11,
+  BW_200HZ = 12,
+  BW_400HZ = 13,
+  BW_800HZ = 14,
+  BW_1600HZ = 15
+};
+
+class ADXL375
+{
+public:
+  ADXL375(struct spi_m_sync_descriptor *SPI, uint8_t CS_PIN);
+  void init();
+  void startMeasuring();
+  AccelData getXYZ();
+  uint8_t readRegister(uint8_t regAddress);
+  void writeRegister(uint8_t regAddress, uint8_t value);
+  void setShockThreshold(uint8_t shockThreshold);
+  void setShockAxes(bool x = true, bool y = true, bool z = true);
+  void startShockDetection();
+  void setDataRate(ADXL375_Bandwidth bw);
+  uint8_t getFIFOBufferSize();
+  void setFIFOMode(uint8_t mode, uint8_t trigger = 0, uint8_t samples = 0);
+
+private:
+  void _multiReadRegister(uint8_t regAddress, uint8_t values[], uint8_t numberOfBytes = 1);
+  struct spi_m_sync_descriptor *SPI;
+  uint8_t CS_PIN;
+};
