0a0b6352362b96aa4850a54ae045cf0611fd9648
Tim Vrakas <tim.vrakas@gmail.com>
2019-03-22 15:45:59 -0700

    Move SPITest code to seperate file

---
+++ src/SPITest.hpp
@@ -0,0 +1,11 @@
+#include <atmel_start.h>
+#include "stdint.h"
+#include "hal_spi_m_sync.h"
+#include "hal_gpio.h"
+#include "printf.h"
+
+bool testBMIGyroSpi();
+bool testBMIAccelSpi();
+bool testADXLSpi();
+bool testBMPSpi();
+bool testSquibSpi();
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,7 @@
 #include "sdtester.c"
 #include "printf.h"
 #include "BMP3xx.hpp"
-
-bool testBMIGyroSpi();
-bool testBMIAccelSpi();
-bool testADXLSpi();
-bool testBMPSpi();
-bool testSquibSpi();
+#include "SPITest.hpp"
 
 int main(void)
 {
@@ -51,17 +46,14 @@ int main(void)
 	while (true)
 	{
 		/*for(int j=0;j<20;j++){
-			
 			gpio_toggle_pin_level(LED4);
-			
 			for(int k=0;k<100;k++){
 				for(int i=0;i<10000;i++){}
-				//gpio_toggle_pin_level(BUZZER);
+				gpio_toggle_pin_level(BUZZER);
 			}
 			printf_("Blink!\n");
-			
-		}
-		*/
+		}*/
+		
 
 		//printf_("SD Test!");	
 		//sdtester();
@@ -100,90 +92,4 @@ int main(void)
 		printf_("%s\n",string);*/
 
 	}
-}
-
-bool testBMIGyroSpi()
-{
-	uint8_t send[] = {0x80, 0x00};
-	uint8_t recv[] = {0x00, 0x00};
-	spi_xfer data = {send, recv, 2};
-	spi_m_sync_disable(&SPI_SENSOR);
-	spi_m_sync_set_mode(&SPI_SENSOR, SPI_MODE_3);
-	spi_m_sync_enable(&SPI_SENSOR);
-	gpio_set_pin_level(GYRO_CS_1, false);
-	spi_m_sync_transfer(&SPI_SENSOR, &data);
-	gpio_set_pin_level(GYRO_CS_1, true);
-	
-	printf_("BMI088-Gyro Response: 0x%x 0x%x\n", data.rxbuf[0], data.rxbuf[1]);
-	return data.rxbuf[1] == 0x0F;
-}
-
-bool testBMIAccelSpi()
-{
-	uint8_t send[] = {0x80, 0x00, 0x00};
-	uint8_t recv[] = {0x00, 0x00, 0x00};
-	spi_xfer data = {send, recv, 3};
-
-	spi_m_sync_disable(&SPI_SENSOR);
-	spi_m_sync_set_mode(&SPI_SENSOR, SPI_MODE_3);
-	spi_m_sync_enable(&SPI_SENSOR);
-
-	gpio_set_pin_level(ACCEL_CS_1, false);
-	spi_m_sync_transfer(&SPI_SENSOR, &data);
-	gpio_set_pin_level(ACCEL_CS_1, true);
-	
-	printf_("BMI088-Accel Response: 0x%x 0x%x 0x%x \n", data.rxbuf[0], data.rxbuf[1], data.rxbuf[2]);
-	return data.rxbuf[2] == 0x1E;
-}
-
-bool testADXLSpi()
-{
-	uint8_t send[] = {0x00};
-	uint8_t recv[] = {0x00};
-	spi_xfer data = {send, recv, 1};
-	spi_m_sync_disable(&SPI_SENSOR);
-	spi_m_sync_set_mode(&SPI_SENSOR, SPI_MODE_3);
-	spi_m_sync_enable(&SPI_SENSOR);
-
-	gpio_set_pin_level(ADXL_CS_1, false);
-	spi_m_sync_transfer(&SPI_SENSOR, &data);
-	gpio_set_pin_level(ADXL_CS_1, true);
-
-	printf_("ADXL375 Response: 0x%x\n", data.rxbuf[0]);
-	return data.rxbuf[0] == 0xE5;
-}
-
-bool testBMPSpi()
-{
-	uint8_t send[] = {0x80, 0x00, 0x00};
-	uint8_t recv[] = {0x00, 0x00, 0x00};
-	spi_xfer data = {send, recv, 3};
-
-	spi_m_sync_disable(&SPI_SENSOR);
-	spi_m_sync_set_mode(&SPI_SENSOR, SPI_MODE_3);
-	spi_m_sync_enable(&SPI_SENSOR);
-
-	gpio_set_pin_level(BMP_CS_1, false);
-	spi_m_sync_transfer(&SPI_SENSOR, &data);
-	gpio_set_pin_level(BMP_CS_1, true);
-	
-	printf_("BMP388 Response: 0x%x 0x%x 0x%x\n", data.rxbuf[0], data.rxbuf[1], data.rxbuf[2]);
-	return data.rxbuf[2] == 0x50;
-}
-
-bool testSquibSpi()
-{
-	uint8_t send = 0x96;
-	uint8_t recv = 0x00;
-	spi_xfer data = {&send, &recv, 1};
-	spi_m_sync_disable(&SPI_SQUIB);
-	spi_m_sync_set_mode(&SPI_SQUIB, SPI_MODE_3);
-	spi_m_sync_enable(&SPI_SQUIB);
-
-	gpio_set_pin_level(SQUIB_CS, false);
-	spi_m_sync_transfer(&SPI_SQUIB, &data);
-	gpio_set_pin_level(SQUIB_CS, true);
-	
-	printf_("Squib Response: 0x%x\n", data.rxbuf[0]);
-	return data.rxbuf[0] == 0x69;
 }
