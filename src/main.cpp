e1824f4f5b53f93b3311803093306aa0534fd278
Tim Vrakas <timv@stanford.edu>
2019-04-02 17:03:20 -0700

    SPI waveform test

--- src/main.cpp
+++ src/main.cpp
@@ -41,25 +41,6 @@ int main(void)
 	spi_m_sync_set_mode(&SPI_SQUIB, SPI_MODE_3);
 	spi_m_sync_enable(&SPI_SQUIB);
 
-	ADXL375 adxl375(&SPI_SENSOR,ADXL_CS_1);
-	adxl375.init();
-	adxl375.startMeasuring();
-
-	BMI088Accel bmi088accel(&SPI_SENSOR,ACCEL_CS_1);
-	bmi088accel.begin();
-
-	BMI088Gyro bmi088gyro(&SPI_SENSOR,GYRO_CS_1);
-	bmi088gyro.begin();
-
-	BMP3xx bmp388(&SPI_SENSOR,BMP_CS_1);
-	bmp388.begin();
-
-	Battery battery(&ADC_0);
-
-	delay_ms(1000);
-
-	Squib squib(&SPI_SQUIB, SQUIB_CS);
-
 	uint8_t leds[] = {LED1,LED2,LED3,LED4};
 
 	uint8_t led = 0;
@@ -68,112 +49,40 @@ int main(void)
 
 	SysTick_Config(48000000/1000);
 
-	while (true)
-	{
-		gpio_toggle_pin_level(leds[led]);
-		led++;
-		led = led % 4;
-		
-		/*
-		for(int k=0;k<100;k++){
-			for(int i=0;i<10000;i++){}
-			gpio_toggle_pin_level(BUZZER);
-		}
-		*/
-		if(fire){
-			squib.fire(CMD_FIRE_1A2A1B2B);
-		}else{
-			squib.fire(CMD_FIRE_NO_SQUIBS);
-		}
-		fire = !fire;
 
-		//printf_("SD Test!");	
-		//sdtester();
+    ADXL375 adxl375(&SPI_SENSOR, ADXL_CS_1);
+    adxl375.init();
+	adxl375.startMeasuring();
 
-		uint32_t timer = millis();
-		
-		squib.getStatus();
-		Squib::Status status = squib.status;
+	BMP3xx bmp388(&SPI_SENSOR, BMP_CS_1);
+    bmp388.begin();
 
-		printf_("squib: %lu\n",millis()-timer);
-		timer = millis();
+    BMI088Accel bmi088accel(&SPI_SENSOR, ACCEL_CS_1);
+    bmi088accel.begin();
 
-		BMI088Accel::Data accel = bmi088accel.readSensor();
+    BMI088Gyro bmi088gyro(&SPI_SENSOR, GYRO_CS_1);
+    bmi088gyro.begin();
 
-		printf_("accel %lu\n",millis()-timer);
-		timer = millis();
+    ADXL375::Data accelHigh;
+    BMP3xx::Data pressure;
+    BMI088Gyro::Data gyro;
+    BMI088Accel::Data accel;
 
-		BMI088Gyro::Data gyro = bmi088gyro.readSensor();
-
-		printf_("gyro: %lu\n",millis()-timer);
-		timer = millis();
-
-		ADXL375::Data accelHigh = adxl375.readSensor();
-
-		printf_("adxl: %lu\n",millis()-timer);
-		timer = millis();
-
-		BMP3xx::Data pressure = bmp388.readSensor();
-
-		printf_("pressure: %lu\n",millis()-timer);
-		timer = millis();
-
-		Battery::cell_voltage_t cells = battery.readVoltage();
-
-		printf_("batt: %lu\n",millis()-timer);
-		timer = millis();
-
-		DynamicJsonDocument doc(1024);
-
-		JsonObject bmi088_json = doc.createNestedObject("BMI088");
-		bmi088_json["temp"] = accel.temp;
-		bmi088_json["time"] = accel.time;
-
-		JsonObject bmi_accel_json = bmi088_json.createNestedObject("Accel");
-		bmi_accel_json["x"] = accel.x;
-		bmi_accel_json["y"] = accel.y;
-		bmi_accel_json["z"] = accel.z;
-
-		JsonObject bmi_gyro_json = bmi088_json.createNestedObject("Gryo");
-		bmi_gyro_json["x"] = gyro.x;
-		bmi_gyro_json["y"] = gyro.y;
-		bmi_gyro_json["z"] = gyro.z;
-
-		JsonObject adxl375_json = doc.createNestedObject("ADXL375");
-		adxl375_json["x"] = accelHigh.x;
-		adxl375_json["y"] = accelHigh.y;
-		adxl375_json["z"] = accelHigh.z;
-
-		JsonObject bmp388_json = doc.createNestedObject("BMP388");
-		bmp388_json["pres"] = pressure.pressure;
-		bmp388_json["temp"] = pressure.temperature;
-
-		JsonObject squib_json = doc.createNestedObject("Squib");
-		squib_json["fen1"] = status.Squib_StatFen1;
-		squib_json["fen2"] = status.Squib_StatFen2;
-		squib_json["1A_res"] = status.Squib_Stat1AResistance;
-		squib_json["1B_res"] = status.Squib_Stat1BResistance;
-		squib_json["2A_res"] = status.Squib_Stat2AResistance;
-		squib_json["2B_res"] = status.Squib_Stat2BResistance;
-
-		JsonObject battery_json = doc.createNestedObject("Battery");
-		battery_json["cellA"] = cells.cellA;
-		battery_json["cellB"] = cells.cellB;
-		battery_json["total"] = cells.total;
-
-		char string[1000];
-		serializeJson(doc,string,sizeof(string));
-
-		printf("JSON: %lu\n",millis()-timer);
-		timer = millis();
-
-		printf_("%s\n",string);
-
-		printf("print: %lu\n",millis()-timer);
-
+    while (true)
+    {
 		delay_ms(50);
-	}
+        //accelHigh = adxl375.readSensor();
+        pressure = bmp388.readSensor();
+        //accel = bmi088accel.readSensor();
+        //gyro = bmi088gyro.readSensor();
+
+        printf_(" High:{%f %f %f} ", accelHigh.x, accelHigh.y, accelHigh.z);
+        printf_(" Pres:{%f %f} ", pressure.pressure, pressure.temperature);
+        printf_(" Gyro:{%f %f %f} ", gyro.x, gyro.y, gyro.z);
+        printf_(" Accel:{%f %f %f %lu} \n", accel.x, accel.y, accel.z, accel.time);
+    }
 }
+
 
 void SysTick_Handler(void){
 	ms++;
