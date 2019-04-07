3f8c162fd1d0dd680abcd3ed78ea8dd96d8ddcba
Tim Vrakas <tim.vrakas@gmail.com>
2019-04-06 18:54:26 -0700

    more sd

---
+++ src/support/diskio_start.c
@@ -0,0 +1,13 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file or main.c
+ * to avoid loosing it when reconfiguring.
+ */
+
+#include "diskio_start.h"
+
+void diskio_init(void)
+{
+}
---
+++ src/support/diskio_start.h
@@ -0,0 +1,26 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file or main.c
+ * to avoid loosing it when reconfiguring.
+ */
+#ifndef DISKIO_START_H
+#define DISKIO_START_H
+
+#include <atmel_start.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+/**
+ * \berif Initialize DISKIO
+ */
+void diskio_init(void);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // DISKIO_START_H
