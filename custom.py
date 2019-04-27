158cfd7e945d5529b31b22c8c5958df8037a9510
Tim Vrakas <tim.vrakas@gmail.com>
2019-04-27 16:47:55 -0700

    cleanup

--- custom.py
+++ custom.py
@@ -18,8 +18,5 @@ import platform
 import subprocess
 
 buildstr = subprocess.check_output(['git', 'describe', '--long', '--dirty', '--tags']).strip() + "-" + getpass.getuser() + "@" + platform.uname()[1] + "-" + datetime.utcnow().strftime('%d/%m/%Y-%H:%M')
-
-
 print("BUILD VERSION: " + buildstr)
-
 env.Append(CPPDEFINES=[("PIO_BUILD", "\\\"" + buildstr + "\\\"")])
