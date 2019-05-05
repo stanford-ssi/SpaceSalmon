e8b3b6c448ebc7bbbb6cf60be4bde71fb90b1224
Tim Vrakas <tim.vrakas@gmail.com>
2019-05-05 03:15:20 -0700

    speed up rebuild

--- post.py
+++ post.py
@@ -1,16 +1,4 @@
-Import("env")
-
-env.Prepend(
-    CCFLAGS=[
-        "-mfloat-abi=hard",
-        "-mfpu=fpv4-sp-d16"
-    ],
-    LINKFLAGS=[
-        "-mfloat-abi=hard",
-        "-mfpu=fpv4-sp-d16"
-    ],
-    LIBS=["arm_cortexM4lf_math"]
-)
+Import("env","projenv")
 
 from datetime import datetime
 import getpass
@@ -19,4 +7,4 @@ import subprocess
 
 buildstr = subprocess.check_output(['git', 'describe', '--long', '--dirty', '--tags']).strip() + "-" + getpass.getuser() + "@" + platform.uname()[1] + "-" + datetime.utcnow().strftime('%d/%m/%Y-%H:%M')
 print("BUILD VERSION: " + buildstr)
-env.Append(CPPDEFINES=[("PIO_BUILD", "\\\"" + buildstr + "\\\"")])
+projenv.Append(CPPDEFINES=[("PIO_BUILD", "\\\"" + buildstr + "\\\"")])
