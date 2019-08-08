3a5f87477ff3bf34c5cf3f9450adbe594b17345e
Tim Vrakas <tim.vrakas@gmail.com>
2019-08-08 13:39:15 -0700

    fix1

--- post.py
+++ post.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import getpass
 import platform
 import subprocess
-
-buildstr = subprocess.check_output(['git', 'describe', '--long', '--dirty', '--tags']).strip() + "-" + getpass.getuser() + "@" + platform.uname()[1] + "-" + datetime.utcnow().strftime('%d/%m/%Y-%H:%M')
+buildstr = "bork"
+#buildstr = subprocess.check_output(['git', 'describe', '--long', '--dirty', '--tags']).strip() + "-" + getpass.getuser() + "@" + platform.uname()[1] + "-" + datetime.utcnow().strftime('%d/%m/%Y-%H:%M')
 print("BUILD VERSION: " + buildstr)
 projenv.Append(CPPDEFINES=[("PIO_BUILD", "\\\"" + buildstr + "\\\"")])
