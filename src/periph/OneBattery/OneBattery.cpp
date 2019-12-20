c373f348b0eae01020aedfdc685b814862806377
Tim Vrakas <tim.vrakas@gmail.com>
2019-12-20 03:43:13 -0600

    Fixed Battery Sensing

--- src/periph/OneBattery/OneBattery.cpp
+++ src/periph/OneBattery/OneBattery.cpp
@@ -12,14 +12,14 @@ OneBattery::OneBattery(ADC& adc): _adc(adc)
 
 OneBattery::cell_voltage_t OneBattery::readVoltage()
 {
-    uint16_t readx0D, readx19;
+    uint16_t readx0E, readx19;
 
-    readx0D = _adc.read(0x0D);
+    readx0E = _adc.read(0x0E);
     readx19 = _adc.read(0x19);
 
     cell_voltage_t result;
 
-    result.cellMain = 3.3F * readx0D / 0.6520 / 1024;
+    result.cellMain = (((float)readx0E / 1024.) * 4.357) - 0.04793; //This is tuned for TGPY SN. 2 (needs to be reworked)
     result.cellBackup = 3.3F * 4 * readx19 / 1024;
 
     return result;
