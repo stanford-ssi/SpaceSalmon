c5bb82b03a2e91f973c78b51977aa79b9cc973e9
Tim Vrakas <tim.vrakas@gmail.com>
2021-06-27 15:37:20 -0700

    guppy 3.2 bringup: pyrofet pins

--- src/guppy/System.hpp
+++ src/guppy/System.hpp
@@ -29,7 +29,7 @@ class System
 { 
 public:
 
-    PyroFets pyrofets = PyroFets(0, 0, 0, 0, adc0);
+    PyroFets pyrofets = PyroFets(31,33,32,34,adc0);
     Pyro &pyro = pyrofets;
 
     ADC adc0 = ADC(ADC0);
