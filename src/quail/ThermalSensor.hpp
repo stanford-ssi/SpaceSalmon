d5cc597aedcffc8f3fbb2b077de6ec39d52569f0
Max Newport <newpomax@gmail.com>
2021-12-21 13:31:25 -0800

    Merged Ahmed's ThermalSensor

--- src/quail/ThermalSensor.hpp
+++ src/quail/ThermalSensor.hpp
@@ -8,7 +8,7 @@
 class ThermalSensor: public Sensor{
     public:
 
-        ThermalSensor(const char* ch_name, Ad7124::InputSel ainp, Ad7124::InputSel ainm);
+        ThermalSensor(const char* ch_name, Ad7124::InputSel ainp);
 
         float convertToFloat(uint32_t adc_dataword);
         void configure();
