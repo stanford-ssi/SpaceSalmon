2c4de94015e1795e73e1fe41302639d156bfc402
Tim Vrakas <timv@stanford.edu>
2021-12-26 22:19:03 -0600

    Better templates

--- src/quail/slate/Array.hpp
+++ src/quail/slate/Array.hpp
@@ -1,14 +1,14 @@
 #include "SlateKey.hpp"
 
-template <typename T, int size>
+template <typename T>
 class Array : public SlateKeyGeneric
 {
+    const std::initializer_list<T>& list;
     public:
-        Array(const std::string id): SlateKeyGeneric(id){};
-        SlateKey<T> *list[size];
-
-        SlateKey<T>& operator[](int index)
+        Array(const std::string id, std::initializer_list<T> keys): SlateKeyGeneric(id), list(keys){};
+        
+        T operator[](int index)
         {
-            return *list[index];
+            return *(list.begin() + index);
         }
-};
+};  
--- src/quail/slate/Slate.cpp
+++ src/quail/slate/Slate.cpp
@@ -4,7 +4,7 @@ Slate slate;
 
 void mains(){
     
-    slate.solenoids.sol1 = true;
+    slate.solenoids[1] = true;
     float f = slate.adc[0];
     slate.squib.arm = true;
     slate.squib.fire[0] = true;
