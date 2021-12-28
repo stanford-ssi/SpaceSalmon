c0dcc833ffc8a61446f900b4227be60d51b71899
Tim Vrakas <timv@stanford.edu>
2021-12-28 17:15:16 -0600

    omg it's working! some cleaning up is in order.

--- src/dummy/slate/Array.hpp
+++ src/dummy/slate/Array.hpp
@@ -5,26 +5,28 @@
 template <class T, size_t N>
 class Array : public SlateKeyGeneric
 {
-public: //THIS IS STILL NOT WORKING....? we could try a tuple? idk.
-    const std::array<std::reference_wrapper<T>, N> list;
-    Array(const std::string id, std::array<std::reference_wrapper<T>, N> keys): SlateKeyGeneric(id), list(keys){};
-    
-    T& operator[](int index)
+public:
+    std::array<T, N> &listref;
+
+    Array(const std::string id, std::array<T, N> &list) : SlateKeyGeneric(id), listref(list){};
+
+    T &operator[](int index)
     {
-        return list.at(index).get();
+        return listref.at(index);
     }
 
-    void dump(JsonVariant dst){
+    void dump(JsonVariant dst)
+    {
         Serial.println("dump array start");
         JsonArray arr = dst.createNestedArray(id);
         Serial.println("dump array B");
-        for(std::reference_wrapper<T> elem : list){
+        for(T& elem : listref){
             Serial.println("dump array Loop A");
-            Serial.println(static_cast<SlateKeyGeneric>(elem.get()).id.c_str());
+            Serial.println(static_cast<SlateKeyGeneric>(elem).id.c_str());
             Serial.println("dump array Loop B");
-            SlateKeyGeneric& key = static_cast<SlateKeyGeneric&>(elem.get());
+            SlateKeyGeneric& key = static_cast<SlateKeyGeneric&>(elem);
             key.dump(arr);
         }
         Serial.println("dump array C");
     }
-};  
+};
