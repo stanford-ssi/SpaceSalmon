d50d79784907db325234a6f7c77960450297b51c
Tim Vrakas <timv@stanford.edu>
2021-12-27 20:37:29 -0600

    closer to dummy working

--- src/dummy/System.cpp
+++ src/dummy/System.cpp
@@ -1,11 +1,8 @@
 #include "System.hpp"
-#include "slate/Slate.hpp"
 
 System sys;
 
 extern "C" void __libc_init_array(void);
-
-void tests();
 
 int main(void)
 {
@@ -22,28 +19,7 @@ int main(void)
 	USBDevice.attach();
 
 	//start all RTOS tasks (this never returns)
-	//vTaskStartScheduler();
-	tests();
+	vTaskStartScheduler();
 }
 
-Slate slate("quail");
 
-void tests(){
-
-	Serial.begin(9600);
-	delay(2000);
-
-    slate.solenoids[1] = true;
-    float f = slate.adc[0];
-    slate.squib.arm = true;
-    slate.squib.fire[0] = true;
-
-    for(auto i : slate.adc.list){
-        float a = i;
-    }
-
-    StaticJsonDocument<1000> doc;
-    JsonVariant variant = doc.to<JsonVariant>();
-    slate.dump(variant);
-    serializeJson(doc,Serial);
-}
--- src/dummy/System.hpp
+++ src/dummy/System.hpp
@@ -4,16 +4,21 @@ class System;
 
 #include <Arduino.h>
 
-#include "SPI.h"
+#include "TestTask.hpp"
+#include "slate/Slate.hpp"
 
 class System
-{ 
+{
 public:
+
+    Slate slate = Slate("quail");
     
     class Tasks
     {
     public:
+        TestTask rx = TestTask(1);
     };
+
     Tasks tasks;
 };
 
