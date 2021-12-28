1f991ea44663683915e846c5d3a0877316e14786
Tim Vrakas <timv@stanford.edu>
2021-12-28 17:35:10 -0600

    cleaned up more

--- src/dummy/slate/Slate.hpp
+++ src/dummy/slate/Slate.hpp
@@ -16,20 +16,15 @@ public:
 
     SlateKey<bool> arm = SlateKey<bool>("arm", false);
     SlateKey<bool> fire = SlateKey<bool>("fire", false);
-   
     
-    class Solenoid : public Array<SlateKey<bool>,5>
-    {
-    public:
-    std::array<SlateKey<bool>,5> list = {
+    std::array<SlateKey<bool>,5> _solenoid = {
         SlateKey<bool>("1", false),
         SlateKey<bool>("2", false),
         SlateKey<bool>("3", false),
         SlateKey<bool>("4", false),
         SlateKey<bool>("5", false)
-    };
-    Solenoid(const std::string id) : Array(id, list){};
-    } solenoid = Solenoid("solenoid");
+    };  
+    Array<SlateKey<bool>,5> solenoid = Array<SlateKey<bool>,5>("solenoid",_solenoid);
     
     Slate(const std::string id) : Container(id, {std::ref(arm), std::ref(fire), std::ref(squib), std::ref(solenoid)}){};
 };
