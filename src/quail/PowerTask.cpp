91d4c56d63c4c7e7827fb7403814219bea4dcb40
Flynn Dreilinger <flynnd@stanford.edu>
2022-01-27 00:25:14 -0800

    oops pins were switched, still aint work

--- src/quail/PowerTask.cpp
+++ src/quail/PowerTask.cpp
@@ -8,13 +8,13 @@ PowerTask::PowerTask(uint8_t priority) : Task(priority, "LED"){
 void PowerTask::activity()
 {
     while(true){
-        uint16_t i_meas = adc0.read(0x02); // AIN[2] current
+        uint16_t i_meas = adc0.read(0x03); // ADC0/AIN[2] is PB09
         // 0.002 volts per amp
         // x100 = 0.2 volts per amp post amplification
         // 3.3V / 0.2 = 16.5A full scale
         float i_batt = i_meas / (UINT16_MAX / 16.5); // amps
         sys.statedata.setBatteryCurrentState(i_batt);
-        uint16_t v_meas = adc0.read(0x03); // AIN[3] voltage
+        uint16_t v_meas = adc0.read(0x03); // ADC0/AIN[3] is PB08
         // Using a voltage divider
         // R2 = 43kΩ
         // R4 = 10kΩ
