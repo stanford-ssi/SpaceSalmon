60cd625f52a44efc56cd4d94f53e48f7b4bab0bb
Michal <mikh3x4@gmail.com>
2022-05-31 19:50:44 -0700

    300min comm timeout while hearbeats not ready

--- src/quail/config.h
+++ src/quail/config.h
@@ -102,7 +102,8 @@ typedef enum {
  // TX RX
  #define ETHERNET_TXRX
  // #define RADIO_TXRX
- #define COMM_RESET 1800000
+//  #define COMM_RESET 1800000 //30 min
+ #define COMM_RESET 18000000 // 300min
 
  // networking
  #define MY_SLATE_PORT 1000
