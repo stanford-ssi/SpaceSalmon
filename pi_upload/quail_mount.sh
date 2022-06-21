de443839f9ddb0b2272bcffc7e435d75a5aa8c37
Michal <mikh3x4@gmail.com>
2022-06-20 17:01:24 -0700

    auto setup

--- pi_upload/quail_mount.sh
+++ pi_upload/quail_mount.sh
@@ -1,7 +1,12 @@
 
-# run to configure
-# echo "21" >  /sys/class/gpio/export
-# echo "out" > /sys/class/gpio/gpio21/directio
+
+# configures if not configured
+if [ ! -d "/sys/class/gpio/gpio21" ]; then
+	echo "21" > /sys/class/gpio/export
+fi
+
+echo "out" > /sys/class/gpio/gpio21/direction
+
 
 echo "0" > /sys/class/gpio/gpio21/value
 sleep 0.1
--- pi_upload/quail_reset.sh
+++ pi_upload/quail_reset.sh
@@ -1,6 +1,10 @@
-# echo "21" >  /sys/class/gpio/export
-# echo "out" > /sys/class/gpio/gpio21/directio
 
+# configures if not configured
+if [ ! -d "/sys/class/gpio/gpio21" ]; then
+	echo "21" > /sys/class/gpio/export
+fi
+
+echo "out" > /sys/class/gpio/gpio21/direction
 
 
 echo "0" > /sys/class/gpio/gpio21/value
