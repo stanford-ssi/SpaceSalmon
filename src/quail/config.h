c90c481f2e2fdb4d54de8f03a551bd069fe353a6
Michal <mikh3x4@gmail.com>
2022-07-01 02:06:54 -0700

    3min comm reset for testing

--- src/quail/config.h
+++ src/quail/config.h
@@ -105,8 +105,8 @@ typedef enum {
  // TX RX
  #define ETHERNET_TXRX
  // #define RADIO_TXRX
+ #define COMM_RESET 180000 //3 min
 //  #define COMM_RESET 1800000 //30 min
- #define COMM_RESET 18000000 // 300min
 
  // networking
  #define MY_SLATE_PORT 1000
