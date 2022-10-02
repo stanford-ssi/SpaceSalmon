03bd9c80f0d7f592cd29f0eb964eefde6496e2b1
Tim Vrakas <timv@stanford.edu>
2022-10-01 21:14:45 -0500

    clean up

--- src/quail/rx_tx_log/CmdServer.cpp
+++ src/quail/rx_tx_log/CmdServer.cpp
@@ -8,14 +8,16 @@
 
 CmdServer::CmdServer(uint8_t priority) : Task(priority, "Ethernet") {}
 
-void CmdServer::activity() {
+void CmdServer::activity()
+{
     vTaskDelay(4000);
     sys.slateServer.bind(MY_SLATE_PORT);
-    
+
     netconn *conn = netconn_new(NETCONN_TCP);
     err_t err = netconn_bind(conn, IP4_ADDR_ANY, MY_CMD_PORT);
 
-    while (err != ERR_OK) {
+    while (err != ERR_OK)
+    {
         netconn_delete(conn);
         vTaskDelay(NETWORKING_DELAY);
         conn = netconn_new(NETCONN_TCP);
@@ -26,48 +28,54 @@ void CmdServer::activity() {
     isSetup = true;
 
     netconn *newconn;
-    while(true) {
+    while (true)
+    {
         // accept a new TCP connection for commands
         err = netconn_accept(conn, &newconn);
 
-        if (err == ERR_OK) {
+        if (err == ERR_OK)
+        {
             netbuf *buf;
-            pb_byte_t* data;
+            pb_byte_t *data;
             uint16_t len;
             quail_telemetry_Message msg;
 
             // while connection is not stale
-            while ((err = netconn_recv(newconn, &buf)) == ERR_OK) {
-                do {
+            while ((err = netconn_recv(newconn, &buf)) == ERR_OK)
+            {
+                do
+                {
                     /*
-                        Going to take a shortcut here, should revise in the future. 
-                        Because the TCP stream is not packetized, we don't know when we've gotten a single full PB message. 
-                        This could result in failures that may cascade into future messages in the stream. 
-                        Or could result in some message being dropped. 
+                        Going to take a shortcut here, should revise in the future.
+                        Because the TCP stream is not packetized, we don't know when we've gotten a single full PB message.
+                        This could result in failures that may cascade into future messages in the stream.
+                        Or could result in some message being dropped.
                         Begs the question of whether TCP is the right choice here.
                     */
-                    netbuf_data(buf, (void**)&data, &len);
+                    netbuf_data(buf, (void **)&data, &len);
                     pb_istream_t rxd_msg = pb_istream_from_buffer(data, len);
-                    pb_decode_ex(&rxd_msg,quail_telemetry_Message_fields, &msg, PB_DECODE_DELIMITED);
+                    pb_decode_ex(&rxd_msg, quail_telemetry_Message_fields, &msg, PB_DECODE_DELIMITED);
 
                     // process command and send ACK
                     bool respond = false;
                     msg_handler(msg, respond);
 
-                    if(respond){
+                    if (respond)
+                    {
                         pb_ostream_t substream = PB_OSTREAM_SIZING;
                         pb_encode_ex(&substream, quail_telemetry_Message_fields, &msg, PB_ENCODE_DELIMITED); // this is very waseful. We're encoding like 4 times here.
                         uint8_t data[substream.bytes_written];
-                        substream = pb_ostream_from_buffer(data,substream.bytes_written);
+                        substream = pb_ostream_from_buffer(data, substream.bytes_written);
                         pb_encode_ex(&substream, quail_telemetry_Message_fields, &msg, PB_ENCODE_DELIMITED);
-                        netconn_write(newconn,data,substream.bytes_written,NETCONN_COPY);
+                        netconn_write(newconn, data, substream.bytes_written, NETCONN_COPY);
                         printf("Resonded with %u \n", substream.bytes_written);
                     }
 
-                    if (err != ERR_OK) {
+                    if (err != ERR_OK)
+                    {
                         PRINT("ETHERNET: cmd tcp connection failed to echo");
                     }
-                } while(netbuf_next(buf) >= 0);
+                } while (netbuf_next(buf) >= 0);
                 netbuf_delete(buf);
             }
 
@@ -78,15 +86,16 @@ void CmdServer::activity() {
     }
 }
 
-err_t CmdServer::msg_handler(quail_telemetry_Message &msg, bool &respond) {
+err_t CmdServer::msg_handler(quail_telemetry_Message &msg, bool &respond)
+{
     switch (msg.which_message)
     {
     case quail_telemetry_Message_reboot_tag:
         printf("Rebooting");
-        vTaskDelay(100); //flush
+        vTaskDelay(100); // flush
         NVIC_SystemReset();
         break;
-    
+
     case quail_telemetry_Message_start_udp_tag:
         PRINT("start udp\n");
         sys.slateServer.connect({msg.message.start_udp.addr}, msg.message.start_udp.port);
@@ -96,7 +105,7 @@ err_t CmdServer::msg_handler(quail_telemetry_Message &msg, bool &respond) {
         PRINT("request meta\n");
         msg.which_message = quail_telemetry_Message_response_metaslate_tag;
         msg.message.response_metaslate.hash = msg.message.request_metaslate.hash;
-        memcpy(msg.message.response_metaslate.metaslate.bytes,telemetry_t::metaslate_blob,sizeof(telemetry_t::metaslate_blob));
+        memcpy(msg.message.response_metaslate.metaslate.bytes, telemetry_t::metaslate_blob, sizeof(telemetry_t::metaslate_blob));
         msg.message.response_metaslate.metaslate.size = sizeof(telemetry_t::metaslate_blob);
         respond = true;
         break;
@@ -115,4 +124,3 @@ err_t CmdServer::msg_handler(quail_telemetry_Message &msg, bool &respond) {
     }
     return RET::OK;
 }
-
--- src/quail/rx_tx_log/CmdServer.hpp
+++ src/quail/rx_tx_log/CmdServer.hpp
@@ -6,31 +6,18 @@
 #include "../config.h"
 #include "cmd.pb.h"
 
-class CmdServer : public Task<2000>{
-    public:
-        CmdServer(uint8_t priority);
+class CmdServer : public Task<2000>
+{
+public:
+    CmdServer(uint8_t priority);
 
-        void activity();
+    void activity();
 
-        void send(const uint8_t* message, uint16_t len) {sendUDP(slateConn, message, len);}
+private:
+    bool isSetup = false;
+    bool udpSetup = false;
 
-        void sendUDP(netconn *conn, const uint8_t* message, uint16_t len);
+    netconn *slateConn = NULL; // NULL is important to ensure it doesn't get fake deleted the first time
 
-        void waitForCmd(char *cmd) {cmdBuf.receive(cmd, DATA_PCKT_LEN, true);};
-        bool cmdAvailable() {return !cmdBuf.empty();}
-
-    private:
-        bool isSetup = false;
-        bool udpSetup = false;
-
-        netconn *slateConn = NULL; //NULL is important to ensure it doesn't get fake deleted the first time
-
-        StrBuffer<10*MAX_CMD_LENGTH> cmdBuf;
-        StaticJsonDocument<META_PCKT_LEN> metaJSON;
-        char metaStr[META_PCKT_LEN];
-
-        void createUDP(netconn *&conn, uint16_t myport, uint16_t client, ip4_addr_t *target = NULL);
-        void createTCP(netconn *&conn, uint16_t myport);
-
-        err_t msg_handler(quail_telemetry_Message &msg, bool &respond);
+    err_t msg_handler(quail_telemetry_Message &msg, bool &respond);
 };
